652c2e428c7968a99d05ff1bdee0b9825b93a803
🍰  🎀  𝒢𝒶𝒷𝓎  🎀  🍰 <gabrielasilvestre74@gmail.com>
2024-12-03 20:49:55 -0600

    Update ejercicio 2,6.py

--- Codigos/BLOQUE2/ejercicio 2,6.py
+++ Codigos/BLOQUE2/ejercicio 2,6.py
@@ -1,6 +1,6 @@
 #Programa.6 Word = "hamster"
 #Fecha de elaboraciÓn: 20210/10/24
-#Elaborado por: ANDREA GUADALUPE JIMENEZ CARDENAS
+#Elaborado por: GABRIELA SILVESTRE VERA 
 
 word="hamster"
 print(word[-1]) #imprime r por comienxo de derecho a izquierdo en negativo
