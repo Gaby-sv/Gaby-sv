438fa9a88dda923aba169f5a1605f90907439c0f
🍰  🎀  𝒢𝒶𝒷𝓎  🎀  🍰 <gabrielasilvestre74@gmail.com>
2024-12-03 20:21:26 -0600

    Create p7.py

---
+++ Codigos/bloque1/p7.py
@@ -0,0 +1,15 @@
+#programa.7 que pida el radio de un circulo y saque sus valores
+#Fecha de elaboraciÓn: 20210/10/15
+#Elaborado por: GABRIELA SILVESTRE VERA
+
+radio = float(input("Introduce el valor del radio del circulo:"))
+
+#calcular el perimetro (circunferencia)
+perimetro = 2 * 3.1416 * radio
+
+#calcula el area
+area = 3.1416 * radio ** 2
+
+#mostrar los resultados 
+print(f"El perimetro del circulo es:{perimetro}")
+print(f"El area del circulo es: {area}")
