b9164b9e92a236bc4f99ee9f4db95309227a4a4d
🍰  🎀  𝒢𝒶𝒷𝓎  🎀  🍰 <gabrielasilvestre74@gmail.com>
2024-12-03 20:47:17 -0600

    Create p1.py

---
+++ Codigos/BLOQUE2/p1.py
@@ -0,0 +1,10 @@
+#Programa.1 Compuerta  NOT
+#Fecha de elaboraciÓn: 20210/10/22
+#Elaborado por: GABRIELA SILVESTRE VERA 
+
+print("Imprime el contrario a TRUE")
+print(not True ) #imprime falso
+print("\n")
+
+print("Imprime la negacion de fALSE")
+print(not False) # imorime verdadero
