fc2bf780197bdd32cd68f41ad73d30197dac803f
🍰  🎀  𝒢𝒶𝒷𝓎  🎀  🍰 <gabrielasilvestre74@gmail.com>
2024-12-03 20:07:04 -0600

    Create p2.py

---
+++ Codigos/bloque1/p2.py
@@ -0,0 +1,6 @@
+#Programa2. programar que solicita el nombre y lo imprime con un mensaje personalizado
+#Fecha de elaboraciÓn: 20210/10/14
+#Elaborado por: GABRIELA SILVESTRE VERA.
+
+nombre = input ("Ingresa tu nombre:")
+print ("Hola" + nombre + ",Que bonito es tu nombre") 
