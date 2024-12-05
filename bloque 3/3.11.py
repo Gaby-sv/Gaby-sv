0d11ff7eb4a8622057f94707160a242a16970aef
🍰  🎀  𝒢𝒶𝒷𝓎  🎀  🍰 <gabrielasilvestre74@gmail.com>
2024-12-05 15:12:56 -0600

    Create 3.11.py

---
+++ bloque 3/3.11.py
@@ -0,0 +1,18 @@
+#Programa.11 Agregando elementos a una lista
+#Elaborado el 31/10/2024
+#Elaborado por : GABRIELASILVESTRE VERA
+
+print("Muestra como unir cadenas con .append une al color Verde ven la lista:")
+colores=["negro", "morado"]
+print(colores)
+colores.append ("Verde")
+print(colores)
+
+#print(colores + "Amarillo") ESTO ES UN ERROR NO SE PUEDE CONCATENAR CADENAS DE TEXTO CON STR Y CADENA DE LISTA
+print("\n")
+
+print("Muestra como unir cadenas con .append une a Caramelos en la lista:")
+dulces =["paletas", "papitas", "Gomitas"]
+print(dulces)
+dulces.append ("Caramelos")
+print(dulces)
