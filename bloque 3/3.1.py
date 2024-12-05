fcd8f9b4bf60c50988b8e02ea8c2f4265f56ad08
🍰  🎀  𝒢𝒶𝒷𝓎  🎀  🍰 <gabrielasilvestre74@gmail.com>
2024-12-05 10:37:49 -0600

    Create 3.1.py

---
+++ bloque 3/3.1.py
@@ -0,0 +1,14 @@
+#Programa.1 ELIFE
+#Elaborado el 28/10/2024
+#Elaborado por : GABRIELA SILVESTRE VERA.
+
+nombre = input("Ingresa la mascota que tienes:  ")
+if "perro" in nombre:
+    print ("Es un perro")
+
+elif "gato" in nombre:
+     print("Es un gato")
+
+
+else:
+     print("Desconocida")
